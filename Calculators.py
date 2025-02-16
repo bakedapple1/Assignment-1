be3a741340e097e01227bb2a307f630341b26c9a
Daniel C <bingrongcao@gmail.com>
2025-02-16 15:47:02 -0500

    Added a python feature that checks if a number is divisible by another number

--- Calculators.py
+++ Calculators.py
@@ -12,5 +12,13 @@ def AddTwoNums(num1, num2):
 def MultiTwoNums(num1, num2):
     return num1 * num2
 
+def IsDivisible(numerator, divisor):
+    return numerator%divisor == 0
+
 print(str(num1) + " + " + str(num2) + " = " + str(AddTwoNums(num1, num2)))
 print(str(num1) + " x " + str(num2) + " = " + str(MultiTwoNums(num1, num2)))
+
+if IsDivisible(num1, num2):
+    print(str(num1) + " is divisible by " + str(num2))
+else:
+    print(str(num1) + " is not divisible by " + str(num2))
